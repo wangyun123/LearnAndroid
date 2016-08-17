6fdfcd24dc86d65276f1a67bdaf7f8b44aee11a5
wangyun <wangyun.ch7@gmail.com>
2016-08-17 21:49:00 +0800

    2016-08-17

--- app/src/main/java/com/example/swty_wy/myapplication/MyContentProvider.java
+++ app/src/main/java/com/example/swty_wy/myapplication/MyContentProvider.java
@@ -6,9 +6,9 @@ import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
 import android.os.CancellationSignal;
-import android.support.annotation.Nullable;
 
 import com.example.swty_wy.myapplication.database.DatabaseHelper;
 
@@ -16,7 +16,7 @@ import com.example.swty_wy.myapplication.database.DatabaseHelper;
  * Created by SWTY-WY on 2016/8/17.
  */
 public class MyContentProvider extends ContentProvider {
-    private static final String TAG = “MyContentProvider”;
+    private static final String TAG = "MyContentProvider";
 
     private SQLiteDatabase sqlDB;
     private DatabaseHelper dbHelper;
@@ -26,13 +26,11 @@ public class MyContentProvider extends ContentProvider {
         return 0;
     }
 
-    @Nullable
     @Override
     public String getType(Uri uri) {
         return null;
     }
 
-    @Nullable
     @Override
     public Uri insert(Uri uri, ContentValues values) {
         sqlDB = dbHelper.getWritableDatabase();
@@ -48,17 +46,33 @@ public class MyContentProvider extends ContentProvider {
     @Override
     public boolean onCreate() {
         dbHelper = new DatabaseHelper(getContext());
-        return false;
+        return (dbHelper == null) ? false : true;
     }
 
-    @Nullable
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        qb.setTables(DatabaseHelper.TABLE_NAME);
+        Cursor c = qb.query(db, projection, selection, null, null, null, sortOrder);
+        c.setNotificationUri(getContext().getContentResolver(), uri);
+        return c;
+    }
+
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder, CancellationSignal cancellationSignal) {
-        return super.query(uri, projection, selection, selectionArgs, sortOrder, cancellationSignal);
+        //return super.query(uri, projection, selection, selectionArgs, sortOrder, cancellationSignal);
+        SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        qb.setTables(DatabaseHelper.TABLE_NAME);
+        Cursor c = qb.query(db, projection, selection, null, null, null, sortOrder);
+        c.setNotificationUri(getContext().getContentResolver(), uri);
+        return c;
     }
 
     @Override
     public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
         return 0;
     }
+
 }
